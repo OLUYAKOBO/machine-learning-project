82d042461c5371fd712362dc0839ca0dddd4eaf8
Yakub Junaid <Junaidyakub28@gmail.com>
2024-10-21 13:00:28 +0100

    created the setup

--- templates.py
+++ templates.py
@@ -2,3 +2,60 @@
 My templates for the project
 
 """
+
+"""
+This file is created in order to list out the files needed for the project and
+ to automate the process of creating the files
+
+
+"""
+
+import logging
+import os
+from pathlib import Path
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+project_name = "Mlops_project"
+
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/_init_.py",
+    f"src/{project_name}/components/_init_.py",
+    f"src/{project_name}/utils/_init_.py",
+    f"src/{project_name}/utils/common.py",
+    f"src/{project_name}/config/_init_.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/_init_.py",
+    f"src/{project_name}/entity/_init_.py",
+    f"src/{project_name}/entity/config_entity.py",
+    f"src/{project_name}/constants/_init_.py",
+    "config/config.yaml",
+    "params.yaml",
+    "schema.yaml",
+    "main.py",
+    "app.py",
+    "Dockerfile",
+    "requirements.txt",
+    "setup.py",
+    "research/trials.ipynb",
+    "templates/index.html",
+    "test.py"
+]
+
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir,filename = os.path.split(filepath)
+
+    if filedir !="":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"creating direcotry {filedir} for the file: {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath,"w") as f:
+            pass
+        logging.info(f"creating empty file: {filepath}")
+
+    else:
+        logging.info(f"{filename} already exists")
